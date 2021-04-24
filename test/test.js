3b68a33c7aaa35b05c2785ccadeeca14ca798e09
RichardDorian <richard.dorian@outlook.com>
2021-04-24 13:24:18 +0200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 const BlaguesAPI = require('../lib/index');
 const blaguesAPI = new BlaguesAPI('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjkwODM1MTMxMjc1MDE4MjcwIiwibGltaXQiOjEwMCwia2V5IjoiU3dlcTZTM0lrOW02bjZOaW03VkltSFNjdXA2SUZhd2hzWDNoVmwxQXBxVDJRZlh3VUciLCJjcmVhdGVkX2F0IjoiMjAyMS0wNC0yM1QxNzozOTo1MSswMDowMCIsImlhdCI6MTYxOTE5OTU5MX0.vdRD7xBnX6R9iIkhVfsBtCDVsX3tKJVxjCUosrFx2HU');
 
+//The token is not valid, I regenerated it :']
+
 test();
 
 async function test() {
