321d2f56f3ff040a2eed1ee0f4d595dd42fc7f7f
Williams Bussat <w.bussat@icloud.com>
2022-06-25 02:25:18 +0200

    feat: ✨ Add count endpoint

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
-export const HOST = 'https://www.blagues-api.fr'
+export const HOST = 'https://www.blagues-api.fr';
 
 export enum Endpoints {
+  COUNT = '/api/count',
   FROM_ID = '/api/id/:id:',
   RANDOM = '/api/random',
-  RANDOM_CATEGORIZED = '/api/type/:type:/random',
+  RANDOM_CATEGORIZED = '/api/type/:type:/random'
 }
-
